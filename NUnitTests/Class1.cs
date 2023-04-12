782b1eaeac200a7065a45b8f8469080d11feb5d5
Ronan Culkin <s00225953@atu.ie>
2023-04-12 18:50:51 +0100

    Add project files.

---
+++ NUnitTests/Class1.cs
@@ -0,0 +1,47 @@
+﻿using REProject;
+using Moq;
+using NUnit.Framework;
+
+
+namespace NUnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void CalculatePremium_ReturnsDiscountedPremium_WhenAgeIs50OrAbove_Urban()
+        {
+            // Arrange
+            var dsMock = new Mock<DiscountService>();
+
+
+            dsMock.Setup(d => d.GetDiscount()).Returns(0.9);
+
+            var insuranceService = new InsuranceService(dsMock.Object);
+
+            // Act
+            var premium = insuranceService.CalculatePremium(50, "urban");
+
+            // Assert
+            Assert.AreEqual(4.5, premium);
+        }
+
+        [Test]
+        public void CalculatePremium_ReturnsDiscountedPremium_WhenAgeIs50OrAbove_Rural()
+        {
+            // Arrange
+            var dsMock = new Mock<DiscountService>();
+
+
+            dsMock.Setup(d => d.GetDiscount()).Returns(0.9);
+
+            var insuranceService = new InsuranceService(dsMock.Object);
+
+            // Act
+            var premium = insuranceService.CalculatePremium(50, "rural");
+
+            // Assert
+            Assert.AreEqual(2.2, premium);
+        }
+    }
+}
---
+++ REProject/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System.Security.Claims;
+
+namespace REProject
+{
+
+    public class InsuranceService
+    {
+        private readonly DiscountService _discountService;
+
+        public InsuranceService(DiscountService discountService)
+        {
+            _discountService = discountService;
+        }
+
+        public double CalculatePremium(int age, string location)
+        {
+            double premium = 0.0;
+
+            if (location == "rural")
+            {
+                if (age >= 18 && age < 30)
+                {
+                    premium = 5.0;
+                }
+                else if (age >= 31)
+                {
+                    premium = 2.50;
+                }
+            }
+            else if (location == "urban")
+            {
+                if (age >= 18 && age <= 35)
+                {
+                    premium = 6.0;
+                }
+                else if (age >= 36)
+                {
+                    premium = 5.0;
+                }
+            }
+
+            double discount = _discountService.GetDiscount();
+            if (age >= 50)
+            {
+                premium *= discount;
+            }
+            premium = Math.Round(premium, 1);
+            return premium;
+        }
+    }
+
+    public class DiscountService
+    {
+        public virtual double GetDiscount()
+        {
+            return 0;
+        }
+    }
+
+}
